7a5a5564a29653ecf92b1c44f5f38989ef599be7
shinbian11 <shinbian11@gmail.com>
2022-05-12 12:46:30 +0900

    다시 jQuery로 rollback + header.html 파일에 도움말 추가하는 JS 추가

--- contents/header.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".heart-like-button");
-console.log("button : ", button);
-button.addEventListener("click", () => {
-  if (button.classList.contains("liked")) {
-    button.classList.remove("liked");
-  } else {
-    button.classList.add("liked");
-  }
-});
