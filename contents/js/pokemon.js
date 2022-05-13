e1635728183afab46c35cd6be52b5c6dd13323b6
bananana0118@gmail.com <bananana0118@gmail.com>
2022-05-13 12:19:40 +0900

    pokemon 수정

--- contents/js/pokemon.js
+++ contents/js/pokemon.js
@@ -333,3 +333,34 @@ function playSound() {
     soundElem.play();
   }
 }
+let src = ["scissors", "rock", "paper"];
+
+let imageIndex = 0;
+//TODO ::
+//가위바위보 구현
+//스타트 버튼이 눌리면 함수 실행
+
+
+//컴퓨터 이미지 회전 애니메이션
+function rpc() {
+    startBtn.value="sad";
+    document.querySelector("#comImg").src = `../img/${src[imageIndex]}.gif`;
+    imageIndex++;
+    if (imageIndex >= src.length) {
+        imageIndex = 0;
+    }
+}
+let nIntervalId;
+
+function changeImage() {
+    if (!nIntervalId) {
+        nIntervalId = setInterval(rpc, 100);
+    }
+}
+
+function stopChangeImage() {
+    clearInterval(nIntervalId);
+    nIntervalId = null;
+}
+startBtn.addEventListener("click", changeImage);
+endBtn.addEventListener("click", stopChangeImage);
